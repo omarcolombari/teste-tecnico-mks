78eaf004ca68e567bc55a39172c46d7f18c1347e
Omar Colombari <omarucolombari@gmail.com>
2022-11-23 18:18:58 -0300

    Feat: criado metodo validateUser no AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await compare(password, user.password);
+
+      if (isPasswordValid) {
+        return user;
+      }
+    }
+
+    throw new UnauthorizedException(
+      'Email address or password provided is incorrect.',
+    );
+  }
+}
