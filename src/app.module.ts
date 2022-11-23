5669d966284d7ff586f7991ccc188101944fef75
Omar Colombari <omarucolombari@gmail.com>
2022-11-22 21:26:41 -0300

    Feat: integrado TypeORM ao projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: false,
+      logging: true,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
+      entities: [
+        process.env.NODE_ENV === 'production'
+          ? __dirname + '/**/*.entity.js'
+          : __dirname + '/**/*.entity.ts',
+      ],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
