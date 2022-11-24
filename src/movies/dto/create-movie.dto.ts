4366e156f7f3462e92d5a27666378822b1a5799f
Omar Colombari <omarucolombari@gmail.com>
2022-11-24 01:50:11 -0300

    Feat: criado CreateMovieDto

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,1 +1,18 @@
-export class CreateMovieDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  duration: number;
+
+  @IsString()
+  classification: string;
+
+  @IsString()
+  synopsis: string;
+
+  @IsString()
+  genres: string;
+}
