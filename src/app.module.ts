951cd2733e469d5468d021a018867705ee643190
Omar Colombari <omarucolombari@gmail.com>
2022-11-24 23:53:13 -0300

    Feat: configurado cache no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +6,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
-import 'dotenv/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -25,6 +25,12 @@ import 'dotenv/config';
           ? { rejectUnauthorized: false }
           : false,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore as any,
+      host: process.env.REDIS_HOST,
+      port: '6379',
     }),
     UsersModule,
     AuthModule,
